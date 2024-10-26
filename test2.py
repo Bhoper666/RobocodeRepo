663806454b225faacdd270e71d34ed8cc4d414b8
Bhoper666 <122364847+Bhoper666@users.noreply.github.com>
2024-10-26 18:30:23 +0300

    Finalizing my project! Its completly done :)

--- test2.py
+++ test2.py
@@ -1,16 +1,25 @@
 import tkinter as tk
+from tkinter import ttk
 
 def buttonpress():
     lbl.config(text='Button pressed!', font=('Times New Roman', 14, 'bold'))
+    root.config(bg='cyan')
+
+def revertback():
+    lbl.config(text='Hello World!', font=('Arial', 16, 'bold italic'))
+    root.config(bg='#f0f0f0')
 
 root = tk.Tk()
 root.title("My Window")
 root.geometry("500x200")
 
-lbl = tk.Label(root, text="Hello World!", font=('Arial', 16, 'bold italic'))
+lbl = ttk.Label(root, text="Hello World!", font=('Arial', 16, 'bold italic'))
 lbl.pack()
 
-butt = tk.Button(root, text='press me Uwu', command=buttonpress)
+butt = ttk.Button(root, text='press me Uwu', command=buttonpress)
 butt.pack()
+
+butt2 = ttk.Button(root, text='revertback', command=revertback)
+butt2.pack()
 
 root.mainloop()
